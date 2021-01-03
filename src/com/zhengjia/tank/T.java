7e2f1e495c828ef1844d462f18725ac554a12737
keniefu <731205328@qq.com>
2021-01-04 00:45:32 +0800

    认识frame窗口
    认识frame窗口

---
+++ src/com/zhengjia/tank/T.java
@@ -0,0 +1,15 @@
+package com.zhengjia.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+    public static void main(String[] args) throws InterruptedException {
+        TankFrame tf = new TankFrame();
+        while (true) {
+            Thread.sleep(50);
+            tf.repaint();
+        }
+    }
+}
---
+++ src/com/zhengjia/tank/TankFrame.java
@@ -0,0 +1,82 @@
+package com.zhengjia.tank;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+
+    int x = 200;
+    int y = 200;
+    public TankFrame() {
+        setSize(800,600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        addKeyListener(new MyKeyListener());
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(x,y,50,50);
+    }
+
+    class MyKeyListener extends KeyAdapter {
+        boolean bl = false;
+        boolean br = false;
+        boolean bu = false;
+        boolean bd = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("key press");
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bl = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = true;
+                    break;
+                default:
+                    break;
+            }
+            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bl = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
